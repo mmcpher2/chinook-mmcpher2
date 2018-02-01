829bceb475373fdecdb46d6bd2873bee2a4df91d
Ryan Mcpherson <mmcpher2@gmail.com>
2018-02-01 15:55:27 -0600

    24 done

---
+++ top_2013_track.sql
@@ -0,0 +1,9 @@
+
+SELECT Name, max("Total Sales")
+FROM(
+SELECT Track.Name, sum(InvoiceLine.TrackId) as "Total Sales"
+FROM Invoice
+JOIN InvoiceLine ON Invoice.InvoiceId = InvoiceLine.InvoiceId
+JOIN Track ON InvoiceLine.TrackId = Track.TrackId
+WHERE Invoice.InvoiceDate between date("2013-01-01") and date("2013-12-31")
+GROUP BY Name);
